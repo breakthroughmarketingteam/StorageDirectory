f6875227b7d35575bd9a2188a87757ba2e35de6f
Diego Salazar <diego@greyrobot.com>
2010-07-21 21:14:40 -0400

    from greybook

--- config/environment.rb
+++ config/environment.rb
@@ -20,14 +20,14 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'liquid'
-   config.gem 'authlogic'
+  # config.gem 'authlogic'
   # config.gem 'facebooker'
   #config.gem 'aws-s3'
   config.gem 'geokit'
+  require 'geokit'
   config.gem 'will_paginate'
-  config.gem 'ajaxful_rating'
-  config.gem 'memcached'
-  require 'memcached'
+  #config.gem 'memcached'
+  #require 'memcached'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/routes.rb
+++ config/routes.rb
@@ -116,11 +116,14 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'show', :title => 'home'
 
   # See how all your routes lay out with "rake routes"
-
+  
+  
+  map.connect ':title', :controller => 'pages', :action => 'show', :title => nil
+  
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':title', :controller => 'pages', :action => 'show', :title => nil
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
