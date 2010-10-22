9ff0aba05aac4f3d1b1335963d5bd4c8507261b8
Diego Salazar <diego@Diego-iMac.local>
2010-10-22 19:39:58 -0400

    m

--- app/models/billing_info.rb
+++ app/models/billing_info.rb
@@ -4,4 +4,8 @@ class BillingInfo < ActiveRecord::Base
   belongs_to :reserver, :foreign_key => 'client_id'
   access_shared_methods
   
+  def obscured_card_number
+    "**** **** **** #{self.card_number[-4]}" if self.
+  end
+  
 end
