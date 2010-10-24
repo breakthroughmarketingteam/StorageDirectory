10e0ab55575877c9bb316644890c8a56313dbcc9
Diego Salazar <diego@Diego-iMac.local>
2010-10-24 19:41:17 -0400

    merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,10 +12,10 @@ class PagesController < ApplicationController
   end
 
   def show
+    @search = Search.new
+    @prev_search = Search.find_by_id session[:search_id] if session[:search_id]
+    
     if params[:title] == 'home' || params[:id].to_i == 1
-      @search = Search.new
-      @prev_search = Search.find_by_id session[:search_id] if session[:search_id]
-      
       #@unit_sizes        = IssnUnitTypeSize.labels
       @unit_features     = IssnUnitTypeFeature.labels
       @facility_features = IssnFacilityFeature.labels
