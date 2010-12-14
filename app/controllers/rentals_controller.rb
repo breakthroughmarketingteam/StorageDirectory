bb456b90aa8c7215df05b3c010e4536c7a869514
Diego Salazar <diego@greyrobot.com>
2010-12-14 18:58:29 -0500

    try ssl

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,7 +5,7 @@ class RentalsController < ApplicationController
   def new
     @search = Search.find_by_id session[:search_id]
     @listing = Listing.find params[:listing_id]
-    @size = @listing.available_sizes.find params[:size_id]
+    @size = @listing.sizes.find params[:size_id]
     
     render :layout => 'bare'
   end
