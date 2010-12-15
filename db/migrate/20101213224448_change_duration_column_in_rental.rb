ad0204b2d50dd42e61b0b4cecefea5320784a0ab
Diego Salazar <diego@greyrobot.com>
2010-12-15 17:30:41 -0500

    migratetion fix - change duration

--- db/migrate/20101213224448_change_duration_column_in_rental.rb
+++ db/migrate/20101213224448_change_duration_column_in_rental.rb
@@ -1,9 +1,9 @@
 class ChangeDurationColumnInRental < ActiveRecord::Migration
   def self.up
-    change_column :rentals, :duration, :integer
+    #change_column :rentals, :duration, :integer
   end
 
   def self.down
-    change_column :rentals, :duration, :string
+    #change_column :rentals, :duration, :string
   end
 end
