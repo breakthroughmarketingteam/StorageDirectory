21634137500f1c71fc97e03485e459d41fe4bd8a
Diego Salazar <diego@greyrobot.com>
2010-12-22 19:47:28 -0500

    blast fix

--- app/models/listing_contact.rb
+++ app/models/listing_contact.rb
@@ -3,6 +3,6 @@ class ListingContact < ActiveRecord::Base
   belongs_to :listing
   access_shared_methods
   
-  named_scope :not_unsub, :conditions => 'unsub IS NULL'
+  named_scope :not_unsub, :conditions => 'unsub IS NULL AND email IS NOT NULL'
   
 end
