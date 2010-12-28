d164f237c4394140fdb280cb3af633120f3a9977
Diego Salazar <diego@greyrobot.com>
2010-12-28 17:35:20 -0500

    migration fix

--- db/migrate/20101227201947_convert_billing_fields_to_binary.rb
+++ db/migrate/20101227201947_convert_billing_fields_to_binary.rb
@@ -1,11 +1,18 @@
 class ConvertBillingFieldsToBinary < ActiveRecord::Migration
   def self.up
     remove_index :billing_infos, :name => :index_billing_infos_on_client_id_and_name_and_card_expiration
-    change_column :billing_infos, :card_number, :binary
-    change_column :billing_infos, :card_type, :binary
-    change_column :billing_infos, :cvv, :binary
-    change_column :billing_infos, :expires_month, :binary
-    change_column :billing_infos, :expires_year, :binary
+
+    remove_column :billing_infos, :card_number
+    remove_column :billing_infos, :card_type
+    remove_column :billing_infos, :cvv
+    remove_column :billing_infos, :expires_month
+    remove_column :billing_infos, :expires_year
+    
+    add_column :billing_infos, :card_number, :binary
+    add_column :billing_infos, :card_type, :binary
+    add_column :billing_infos, :cvv, :binary
+    add_column :billing_infos, :expires_month, :binary
+    add_column :billing_infos, :expires_year, :binary
   end
 
   def self.down
