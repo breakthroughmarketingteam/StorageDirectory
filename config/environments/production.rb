4f95535eaf7d2952a3ce4b2e46de22e4400539d7
Diego Salazar <diego@greyrobot.com>
2010-12-30 17:33:55 -0500

    subdomain fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,8 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = false
 config.action_view.cache_template_loading            = true
 
+config.action_controller.session[:domain] = '.usselfstoragelocator.com'
+
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
