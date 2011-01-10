580a68100878a03f06915621d9ddb20b87efd5a3
Diego Salazar <diego@greyrobot.com>
2011-01-10 11:28:24 -0500

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   
   ssl_required :index, :show, :new, :create, :edit, :update, :destroy
-  before_filter :get_models_paginated, :only => :index
   before_filter :get_model, :only => [:show, :new, :edit, :update, :destroy]
   before_filter :get_roles, :only => [:index, :new, :edit, :create]
   before_filter :get_default_role, :only => :new
   
   def index
+    @users = User.all :conditions => { :type => 'User' }
     render :layout => false if request.xhr?
   end
   
@@ -22,11 +22,26 @@ class UsersController < ApplicationController
     @form = Form.find(params[:fid]) unless params[:fid].blank?
     @user = User.new(params[:user])
     
-    if @user.save
-      flash[:notice] = 'Great! Thanks for signing up!'
-      redirect_back_or_default user_path(@user)
-    else
-      render :action => :new
+    respond_to do |format|
+      format.html do
+        if @user.save
+          flash[:notice] = 'Great! Thanks for signing up!'
+          redirect_back_or_default user_path(@user)
+        else
+          render :action => :new
+        end
+      end
+      
+      format.js do
+        if @user.save
+          flash.now[:notice] = 'Great! Thanks for signing up!'
+          @users = User.all :conditions => { :type => 'User' }
+          render :action => 'index', :layout => false
+        else
+          flash.now[:error] = model_errors @user
+          render :action => 'edit', :layout => false
+        end
+      end
     end
   end
 
