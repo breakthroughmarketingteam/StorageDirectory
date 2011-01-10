ad373024c9cb11143be3f0ce8bdde5bdf4ed2d3f
Diego Salazar <diego@greyrobot.com>
2011-01-10 16:19:56 -0500

    m

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,8 +1,8 @@
 class AjaxController < ApplicationController
   
   skip_before_filter :init
-  ssl_required :get_client_stats, :destroy
-  ssl_allowed :get_partial, :get_multipartial, :modeL_method
+  ssl_required :get_client_stats, :destroy, :modeL_method
+  ssl_allowed :get_partial, :get_multipartial
   before_filter :validate_params, :except => [:find_listings, :get_client_stats, :get_cities]
   before_filter :_get_model, :only => [:get_model, :get_listing, :update, :destroy, :get_multipartial, :model_method]
   before_filter :_get_model_class, :only => [:find, :get_listing, :get_attributes, :model_method]
