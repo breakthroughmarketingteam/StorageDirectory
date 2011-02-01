c0ad9eb93724fe505eac77c787046a0bb0f0728e
Diego Salazar <diego@greyrobot.com>
2011-02-01 15:56:46 -0500

    debug www_redir

--- app/metal/www_redir.rb
+++ app/metal/www_redir.rb
@@ -3,7 +3,9 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 
 class WwwRedir
   def self.call(env)
+    raise env.pretty_inspect
     host = env['HTTP_HOST']
+    
     if host['www'] || host['secure']
       [301, { 'Location' => request.url.sub('//www.', '//')}, ['301 Permanent Redirect']]
     else
