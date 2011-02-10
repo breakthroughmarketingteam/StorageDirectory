31ecfe2d90032ea785495e0266fe6b319fae623c
Diego Salazar <diego@greyrobot.com>
2011-02-10 13:52:38 -0500

    acts as csv

--- vendor/plugins/acts_as_csv/test/acts_as_csv_test.rb
+++ vendor/plugins/acts_as_csv/test/acts_as_csv_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
-
-class SharedModelMethodsTest < ActiveSupport::TestCase
+class ActsAsCsvTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
