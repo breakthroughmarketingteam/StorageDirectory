6f89d8587b345e1476535f272a91a8bf80c93e2c
Diego Salazar <diego@greyrobot.com>
2011-02-28 17:12:34 -0500

    map marker

--- config/environment.rb
+++ config/environment.rb
@@ -37,6 +37,7 @@ Rails::Initializer.run do |config|
   config.gem 'dalli'
   config.gem 'ajaxful_rating'
   config.gem 'ssl_requirement'
+  config.gem 'rmagick'
   config.gem 'delayed_job', :version => '~>2.0.4'
   config.gem "heroscale"
   require "heroscale"
