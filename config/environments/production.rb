21f56ccb2abe5384c892b5f486fa0f597c38e89c
Diego Salazar <diego@greyrobot.com>
2011-03-08 13:30:04 -0500

    trying to fix domain cookie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,12 +9,6 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-begin
-  config.action_controller.session = { :domain => '.usselfstoragelocator.com' }
-rescue
-  config.action_controller.session[:domain] = '.usselfstoragelocator.com'
-end
-
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
