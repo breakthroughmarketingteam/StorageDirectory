6f59b0b5a5ba33cccec599bc9c9709f7a9f65d3f
Diego Salazar <diego@greyrobot.com>
2011-03-10 15:39:00 -0500

    rentalizer fix

--- app/metal/rentalizer.rb
+++ app/metal/rentalizer.rb
@@ -16,7 +16,7 @@ class Rentalizer
           data = params[:multi_params].split('-').map do |str|
             p = str.split('x')
             listing = Listing.find p[0].to_i
-            size    = listing.sizes.find_by_id p[1].to_i if p[1] == 'undefined' || p[1] != 'undefined'
+            size    = listing.sizes.find_by_id p[1].to_i if p[1] || p[1] != 'undefined'
             special = listing.predefined_specials.find_by_id p[2].to_i if p[2] || p[2] != 'undefined'
             
             rental_calc params, listing, size, special, true
