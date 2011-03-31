b1b0bc34c067c6e5cd87132c51af75eb611d067c
Diego Salazar <diego@greyrobot.com>
2011-03-31 17:20:25 -0400

    commented out jammit

--- config/routes.rb
+++ config/routes.rb
@@ -203,7 +203,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   
-  Jammit::Routes.draw(map)
+  #Jammit::Routes.draw(map)
   
   map.connect ':title', :controller => 'pages', :action => 'show'
   map.connect ':controller/:action/:id'
