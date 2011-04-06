600f517fdadd42e2287ab29098b59a3d6491cdb7
Diego Salazar <diego@greyrobot.com>
2011-04-06 16:29:23 -0400

    images

--- app/controllers/info_requests_controller.rb
+++ app/controllers/info_requests_controller.rb
@@ -21,7 +21,7 @@ class InfoRequestsController < ApplicationController
     @info_request = @listing.info_requests.build params[:info_request].merge(:status => 'pending').merge(params[:reserver]).merge(params[:mailing_address])
     
     if @listing.save
-      @info_request.deliver_emails
+      @info_request.delay.deliver_emails
       render :json => { :success => true, :data => render_to_string(:partial => 'info_requests/done') }
     else
       render :json => { :success => false, :data => model_errors(@info_request) }
