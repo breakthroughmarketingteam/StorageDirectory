728d92b0c66ab19ec6cf04d841b0897efd6444cb
Diego Salazar <diego@greyrobot.com>
2011-05-02 18:59:03 -0400

    rm strongbox

--- app/models/billing_info.rb
+++ app/models/billing_info.rb
@@ -6,10 +6,10 @@ class BillingInfo < ActiveRecord::Base
   @@credit_cards = ['Visa', 'Amex', 'MasterCard', 'Discover']
   cattr_reader :credit_cards
   
-  encrypt_with_public_key [:card_number, :card_type, :cvv, :expires_month, :expires_year], :key_pair => File.join(RAILS_ROOT,'cert','keypair3.pem')
+  #encrypt_with_public_key [:card_number, :card_type, :cvv, :expires_month, :expires_year], :key_pair => File.join(RAILS_ROOT,'cert','keypair3.pem')
   
   def obscured_card_number
-    cnum = self.card_number.decrypt
+    cnum = self.card_number #.decrypt
     "**** **** **** #{cnum[cnum.size - 4, cnum.size]}" unless cnum.blank?
   end
   
