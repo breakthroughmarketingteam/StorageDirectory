07ede26e013dbfe7569e4bc0d3335368290ad28c
Diego Salazar <diego@greyrobot.com>
2011-05-12 13:55:21 -0400

    help redir in index

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -5,6 +5,8 @@ class HelpsController < ApplicationController
   before_filter :get_model, :only => [:new, :edit, :update, :destroy]
   
   def index
+    redirect_to help_with_path('coming-soon') if !user_is_a?('admin', 'staff')
+    
     @page = Page.find_by_title 'Help'
     
     respond_to do |format|
