e200baee7ae4354567fc1134755a7b85a2724561
Diego Salazar <diego@greyrobot.com>
2011-05-24 16:10:53 -0400

    trial ends test

--- app/controllers/unsubs_controller.rb
+++ app/controllers/unsubs_controller.rb
@@ -7,7 +7,7 @@ class UnsubsController < ApplicationController
     @model = params[:class_name].constantize.find_by_email params[:email] rescue nil
 
     if @model
-      @unsub = model.unsubs.create :name => params[:list]
+      @unsub = @model.unsubs.create :name => params[:list]
       flash[:notice] = "You have been unsubscribe from the list: #{@unsub.name.titleize}"
     else
       flash[:error] = "Could not find that subscriber. Is the URL correct?"
